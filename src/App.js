62479b09801d4ce6babcad1f5f17030ff87f8f8f
Yigit <tahayigityilmaz@gmail.com>
2019-11-26 12:41:22 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bye 3245325!
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React qqqq
         </a>
       </header>
     </div>
